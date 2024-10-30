51faf5b608ecbaf5a24ff5be246207d0c43e3621
andrei <aevstafev2-c@my.cityu.edu.hk>
2024-10-30 17:14:27 +0800

    initial commit

---
+++ webapp_frontend/android/app/src/main/kotlin/com/example/webapp_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.webapp_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ webapp_frontend/lib/utils/secure_storage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  static final SecureStorage _instance = SecureStorage._internal();
+  factory SecureStorage() => _instance;
+
+  SecureStorage._internal();
+
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<void> write(String key, String value) async {
+      await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> read(String key) async {
+    return await _storage.read(key: key);
+  }
+
+  Future<Map<String, String>> readAll() async {
+    return await _storage.readAll();
+  }
+
+  Future<void> delete(String key) async {
+    await _storage.delete(key: key);
+  }
+
+  Future<void> deleteAll() async {
+    await _storage.deleteAll();
+  }
+}
